9463bbba611975bce0eb0b3c6284be2e342d86ad
malauzet <30903872+malauzet@users.noreply.github.com>
2026-06-22 11:58:23 +0200

    TP fini

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,19 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+
+        byte a = 1;
+        short b = 2;
+        int c = 3;
+        long d = 4L;
+        float e = 5.1f;
+        double f = 6.6;
+        char g = 'g';
+        boolean h = true;
+        String i = "Mon boolean est: ";
+        String randomString = "Voici le résultat d’un calcul :\n 1+5=6";
+
+        System.out.println(randomString);
+    }
+}
